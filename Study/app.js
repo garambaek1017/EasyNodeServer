3647c18433783247b905f85c13a63611a8e881af
GaramBaek <bgr1017.kr@gmail.com>
2023-05-30 00:52:31 +0900

    로그인 페이지 추가

--- Study/app.js
+++ Study/app.js
@@ -1,37 +1,43 @@
-var http = require('http');
-var fs = require('fs');
 
-var server = http.createServer();
+// Express 기본 모듈 불러오기
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
 
-var port = 3000;
+// Express의 미들웨어 불러오기
+var bodyParser = require('body-parser')
+  , static = require('serve-static');
 
-server.listen(port, function(){
-    console.log("웹 서버가 시작되었습니다 %d", port);
+// 익스프레스 객체 생성
+var app = express();
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json())
+
+app.use(static(path.join(__dirname, 'public')));
+
+// 미들웨어에서 파라미터 확인
+app.use(function(req, res, next) {
+	console.log('첫번째 미들웨어에서 요청을 처리함.');
+
+	var paramId = req.body.id || req.query.id;
+	var paramPassword = req.body.password || req.query.password;
+	
+	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+	res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+	res.write('<div><p>Param id : ' + paramId + '</p></div>');
+	res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+	res.end();
 });
 
-server.on('connection', function(socket){
-    var addr = socket.address();
 
-    console.log('클라이언트가 접속했습니다. : %s, %d',addr.address, addr.port);
-
+// Express 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-// on 메서드는 이벤트를 처리할 때 사용하는 가장 기본적인 메서드     
-server.on('request', function(req,res){
-    console.log('클라이언트 요청이 들어왔습니다.');
-    
-
-    var filename = "summer_draw.png";
-    
-    fs.readFile(filename, function(err, data){
-        res.writeHead(200, {"Content-type":"image/png; charset=utf-8"});
-        res.write(data);
-        res.end();
-    });
-
-});
-
-server.on('close', function(){
-    console.log('서버가 종료됩니다.');
-});
-
